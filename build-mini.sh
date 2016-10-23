9a557c0fab7e76f922f38354d08dcec284784e34
Rocky Wei <finscn@gmail.com>
2016-10-24 02:07:37 +0800

    update

--- build-mini.sh
+++ build-mini.sh
@@ -1,2 +1,3 @@
-# -e extras 
-npm run build -- -e mesh -e loaders -e extract -e interaction -e accessibility
+# core, extract, extras, filters, mesh, particles, prepare
+# -e mesh -e extract -e extras
+npm run build -- -e loaders -e interaction -e accessibility
