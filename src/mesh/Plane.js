660f5eef9efd0d363e81dd854806b7dc9abfed57
Rocky Wei <finscn@gmail.com>
2016-12-20 01:10:46 +0800

    add points & support texture changing

--- src/mesh/Plane.js
+++ src/mesh/Plane.js
@@ -1,4 +1,7 @@
 import Mesh from './Mesh';
+import * as core from '../core';
+
+const Point = core.Point;
 
 /**
  * The Plane allows you to draw a texture across several points and them manipulate these points
@@ -27,6 +30,17 @@ export default class Plane extends Mesh
         super(texture);
 
         /**
+         * The anchor sets the origin point of the texture.
+         * The default is 0,0 this means the texture's origin is the top left
+         * Setting the anchor to 0.5,0.5 means the texture's origin is centered
+         * Setting the anchor to 1,1 would mean the texture's origin point will be the bottom right corner
+         *
+         * @member {PIXI.ObservablePoint}
+         * @private
+         */
+        this._anchor = new core.ObservablePoint(this._onAnchorUpdate, this);
+
+        /**
          * Tracker for if the Plane is ready to be drawn. Needed because Mesh ctor can
          * call _onTextureUpdated which could call refresh too early.
          *
@@ -39,7 +53,30 @@ export default class Plane extends Mesh
         this.verticesY = verticesY || 10;
 
         this.drawMode = Mesh.DRAW_MODES.TRIANGLES;
+
+        this.initPoints();
         this.refresh();
+    }
+
+    /**
+     * Initialize points
+     *
+     */
+    initPoints()
+    {
+        this.points = [];
+        for (let i = 0; i < this.verticesY; i++)
+        {
+            const row = [];
+            this.points.push(row);
+            for (let j = 0; j < this.verticesX; j++)
+            {
+                const point = new Point(j, i);
+                point.originalX = point.x;
+                point.originalY = point.y;
+                row.push(point);
+            }
+        }
     }
 
     /**
@@ -53,7 +90,15 @@ export default class Plane extends Mesh
         const colors = [];
         const uvs = [];
         const indices = [];
-        const texture = this.texture;
+
+        const texture = this._texture;
+        // const trim = texture.trim;
+        const orig = texture.orig;
+        const anchor = this._anchor;
+
+        // calculate the space around pivot point.
+        this._pivotX = orig.width * anchor.x;
+        this._pivotY = orig.height * anchor.y;
 
         const segmentsX = this.verticesX - 1;
         const segmentsY = this.verticesY - 1;
@@ -61,20 +106,37 @@ export default class Plane extends Mesh
         const sizeX = texture.width / segmentsX;
         const sizeY = texture.height / segmentsY;
 
+        let sizeChanged = false;
+
+        if (sizeX !== this._sizeX || sizeY !== this._sizeY)
+        {
+            this._sizeX = sizeX;
+            this._sizeY = sizeY;
+            sizeChanged = true;
+        }
+
         for (let i = 0; i < total; i++)
         {
             if (texture._uvs)
             {
                 const x = (i % this.verticesX);
                 const y = ((i / this.verticesX) | 0);
+                const point = this.points[y][x];
 
-                verts.push((x * sizeX),
-                           (y * sizeY));
+                if (sizeChanged)
+                {
+                    point.x = x * sizeX;
+                    point.y = y * sizeY;
+                    point.originalX = point.x;
+                    point.originalY = point.y;
+                }
+
+                verts.push(point.x - this._pivotX, point.y - this._pivotY);
 
                 // this works for rectangular textures.
                 uvs.push(
-                    texture._uvs.x0 + ((texture._uvs.x1 - texture._uvs.x0) * (x / (this.verticesX - 1))),
-                    texture._uvs.y0 + ((texture._uvs.y3 - texture._uvs.y0) * (y / (this.verticesY - 1)))
+                    texture._uvs.x0 + ((texture._uvs.x1 - texture._uvs.x0) * (x / segmentsX)),
+                    texture._uvs.y0 + ((texture._uvs.y3 - texture._uvs.y0) * (y / segmentsY))
                 );
             }
             else
@@ -82,8 +144,6 @@ export default class Plane extends Mesh
                 uvs.push(0);
             }
         }
-
-        //  cons
 
         const totalSub = segmentsX * segmentsY;
 
@@ -107,7 +167,23 @@ export default class Plane extends Mesh
         this.colors = new Float32Array(colors);
         this.indices = new Uint16Array(indices);
 
-        this.indexDirty = true;
+        this.dirty++;
+        this.indexDirty++;
+    }
+
+    /**
+     * Called when the anchor position updates.
+     *
+     * @private
+     */
+    _onAnchorUpdate()
+    {
+        this._transformID = -1;
+
+        if (this._ready)
+        {
+            this.refresh();
+        }
     }
 
     /**
@@ -117,7 +193,7 @@ export default class Plane extends Mesh
      */
     _onTextureUpdate()
     {
-        Mesh.prototype._onTextureUpdate.call(this);
+        super._onTextureUpdate();
 
         // wait for the Plane ctor to finish before calling refresh
         if (this._ready)
@@ -126,4 +202,27 @@ export default class Plane extends Mesh
         }
     }
 
+    /**
+     * The anchor sets the origin point of the texture.
+     * The default is 0,0 this means the texture's origin is the top left
+     * Setting the anchor to 0.5,0.5 means the texture's origin is centered
+     * Setting the anchor to 1,1 would mean the texture's origin point will be the bottom right corner
+     *
+     * @member {PIXI.ObservablePoint}
+     * @memberof PIXI.Sprite#
+     */
+    get anchor()
+    {
+        return this._anchor;
+    }
+
+    /**
+     * Copies the anchor to the plane.
+     *
+     * @param {number} value - The value to set to.
+     */
+    set anchor(value)
+    {
+        this._anchor.copy(value);
+    }
 }
