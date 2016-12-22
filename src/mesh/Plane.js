14be3d1b62582e96f7dc25b67a651bbad33feb6a
Rocky Wei <finscn@gmail.com>
2016-12-23 00:53:44 +0800

    could pass canvas

--- src/mesh/Plane.js
+++ src/mesh/Plane.js
@@ -84,7 +84,6 @@ export default class Plane extends Mesh
         const orig = texture.orig;
         const anchor = this._anchor;
 
-
         this._pivotX = orig.width * anchor.x;
         this._pivotY = orig.height * anchor.y;
 
