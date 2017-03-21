655d67d8837063b40062a030503a20d1515b3a98
Rocky Wei <finscn@gmail.com>
2017-03-22 00:33:28 +0800

    add ColorReplacement Filter

--- src/extensions/filters/index.js
+++ src/extensions/filters/index.js
@@ -10,3 +10,4 @@ export { default as PerspectiveFilter } from './perspective/PerspectiveFilter';
 export { default as RadialBlurXFilter } from './radialblur/RadialBlurXFilter';
 export { default as RadialBlurYFilter } from './radialblur/RadialBlurYFilter';
 export { default as RadialBlurFilter } from './radialblur/RadialBlurFilter';
+export { default as ColorReplacementFilter } from './colorreplacement/ColorReplacementFilter';
