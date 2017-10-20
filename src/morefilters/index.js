033905965c716f13c7a526b8dfe0d69241e87d12
Rocky Wei <finscn@gmail.com>
2017-10-21 04:16:06 +0800

    move KawaseBlurFilter & RadialBlurFilter to pixi-filters

--- src/morefilters/index.js
+++ src/morefilters/index.js
@@ -1,10 +1,6 @@
 /**
  * @namespace PIXI.filters
  */
-export { default as KawaseBlurFilter } from './kawaseblur/KawaseBlurFilter';
 export { default as ShockwaveFilter } from './shockwave/ShockwaveFilter';
 export { default as PerspectiveFilter } from './perspective/PerspectiveFilter';
-export { default as RadialBlurXFilter } from './radialblur/RadialBlurXFilter';
-export { default as RadialBlurYFilter } from './radialblur/RadialBlurYFilter';
-export { default as RadialBlurFilter } from './radialblur/RadialBlurFilter';
 export { default as TestShaderFilter } from './testshader/TestShaderFilter';
