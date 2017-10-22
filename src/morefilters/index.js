8bf76fa2946117c2ae7f9f01e69073b203985668
Rocky Wei <finscn@gmail.com>
2017-10-22 23:50:57 +0800

    move ShockwaveFilter & ShockwaveFilter to pixi-filters

--- src/morefilters/index.js
+++ src/morefilters/index.js
@@ -1,6 +1,5 @@
 /**
  * @namespace PIXI.filters
  */
-export { default as ShockwaveFilter } from './shockwave/ShockwaveFilter';
 export { default as PerspectiveFilter } from './perspective/PerspectiveFilter';
 export { default as TestShaderFilter } from './testshader/TestShaderFilter';
