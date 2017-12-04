e1f992ce652f09583b4be5126649ee92eb1db57c
Rocky Wei <finscn@gmail.com>
2017-12-04 13:28:28 +0800

    refactor Shader

--- src/core/Shader.js
+++ src/core/Shader.js
@@ -32,21 +32,18 @@ function checkPrecision(src, def)
  */
 export default class Shader extends GLShader
 {
-    /* eslint-disable max-len */
-
     /**
      *
      * @param {WebGLRenderingContext} gl - The current WebGL rendering context
      * @param {string|string[]} vertexSrc - The vertex shader source as an array of strings.
      * @param {string|string[]} fragmentSrc - The fragment shader source as an array of strings.
      * @param {string} precision - The float precision of the shader. Options are 'lowp', 'mediump' or 'highp'.
-     * @param {object} attributeLocations - A key value pair showing which location eact attribute should sit, e.g. {position:0, uvs:1}.
+     * @param {object} attributeLocations - A key value pair showing which location eact attribute should sit.
+                       e.g. {position:0, uvs:1}.
      */
     constructor(gl, vertexSrc, fragmentSrc, precision, attributeLocations)
     {
-        super(gl, checkPrecision(vertexSrc, settings.PRECISION_VERTEX),
-            checkPrecision(fragmentSrc, settings.PRECISION_FRAGMENT), precision, attributeLocations);
+        super(gl, checkPrecision(vertexSrc, precision || settings.PRECISION_VERTEX),
+            checkPrecision(fragmentSrc, precision || settings.PRECISION_FRAGMENT), null, attributeLocations);
     }
-
-    /* eslint-enable max-len */
 }
