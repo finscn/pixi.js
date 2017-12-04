735834dd415ef2cbb27f4374daf0909949609ebf
Rocky Wei <finscn@gmail.com>
2017-12-05 04:50:27 +0800

    cosmetic

--- src/ext-plugins/shader-particle/status.frag.js
+++ src/ext-plugins/shader-particle/status.frag.js
@@ -8,31 +8,21 @@ uniform vec2 uViewSize;
 
 const float gravity = 0.75;
 
-float rand(vec2 co)
-{
-    float a = 12.9898;
-    float b = 78.233;
-    float c = 43758.5453;
-    float dt= dot(co.xy ,vec2(a,b));
-    float sn= mod(dt,3.14);
-    return fract(sin(sn) * c);
-}
-
 void main(void)
 {
     vec4 position = texture2D(uStatusIn, vTextureCoord);
+
     position.xy += position.zw;
     position.w += gravity;
 
     if(position.y > uViewSize.y)
     {
         position.y = uViewSize.y;
-        position.w *= -0.85;
-
-        if(position.w > -20.0)
+        if(position.w > 25.0)
         {
-            position.w = rand(vTextureCoord) * -32.0;
+            position.w = 25.0;
         }
+        position.w *= -1.0;
     }
 
     if(position.x > uViewSize.x)
@@ -40,8 +30,7 @@ void main(void)
         position.x = uViewSize.x;
         position.z *= -1.0;
     }
-
-    if(position.x < 0.0)
+    else if(position.x < 0.0)
     {
         position.x = 0.0;
         position.z *= -1.0;
