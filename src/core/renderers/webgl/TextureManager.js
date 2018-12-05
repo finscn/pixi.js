6c58e66b4f11c946fd8c32fa9e419d347109c87e
finscn <finscn@gmail.com>
2018-12-05 14:25:09 +0800

    update to 6a90c33

--- src/core/renderers/webgl/TextureManager.js
+++ src/core/renderers/webgl/TextureManager.js
@@ -212,13 +212,14 @@ export default class TextureManager
             return;
         }
 
-        const uid = this.renderer.CONTEXT_UID;
+        const renderer = this.renderer;
+        const uid = renderer.CONTEXT_UID;
         const glTextures = texture._glTextures;
         const glRenderTargets = texture._glRenderTargets;
 
         if (glTextures[uid])
         {
-            this.renderer.unbindTexture(texture);
+            renderer.unbindTexture(texture);
 
             glTextures[uid].destroy();
             texture.off('update', this.updateTexture, this);
@@ -239,6 +240,11 @@ export default class TextureManager
 
         if (glRenderTargets && glRenderTargets[uid])
         {
+            if (renderer._activeRenderTarget === glRenderTargets[uid])
+            {
+                renderer.bindRenderTarget(renderer.rootRenderTarget);
+            }
+
             glRenderTargets[uid].destroy();
             delete glRenderTargets[uid];
         }
